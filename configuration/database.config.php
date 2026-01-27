bf52e4e6695a4ea77f1f7193c7ce41a64493e628
Sam@Unique <aymantheremkus11@gmail.com>
2026-01-27 22:45:20 +0300

    Mess With .gitignore

--- configuration/database.config.php
+++ configuration/database.config.php
@@ -4,4 +4,5 @@ use Tempest\Database\Config\SQLiteConfig;
 
 use function Tempest\root_path;
 
+// Create a database folder in the root of the project if you are using SQLite.
 return new SQLiteConfig(path: root_path('database/database.sqlite'));
